0ae067dd036deb57e18025c8bc699e4f49617a30
Бушуев Роман <1997pom@gmail.com>
2023-04-23 22:22:49 +1000

    work docker

--- app/routing.py
+++ app/routing.py
@@ -2,7 +2,7 @@ from django.urls import path
 from app.consumers import GraphConsumerCmn, GraphConsumerAi, DetailsConsumerCmn
 
 ws_urlpatterns = [
-    # path('ws/graph/', GraphConsumerCmn.as_asgi()),
+    path('ws/graph/', GraphConsumerCmn.as_asgi()),
     path('ws/table/', GraphConsumerAi.as_asgi()),
-    # path('ws/details/', DetailsConsumerCmn.as_asgi()),
+    path('ws/details/', DetailsConsumerCmn.as_asgi()),
 ]
