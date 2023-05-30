871c447d443353983f57418264b819f06c9d5a4a
Бушуев Роман <1997pom@gmail.com>
2023-05-30 14:56:09 +1000

    deploy render.com use sqlite3

--- djangoProjectDiagnostics/settings.py
+++ djangoProjectDiagnostics/settings.py
@@ -95,24 +95,24 @@ ASGI_APPLICATION = 'djangoProjectDiagnostics.asgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'diagnostics',
-        'USER': 'postgres',
-        'PASSWORD': '12345678',
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'diagnostics',
+#         'USER': 'postgres',
+#         'PASSWORD': '12345678',
+#         'HOST': os.environ.get('DB_HOST'),
+#         'PORT': '5432',
+#     }
+# }
 
 
 # CHANNEL_LAYERS = {
